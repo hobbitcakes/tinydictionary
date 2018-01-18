7930f66d28e83b55e12e484e2fbcf1dc062b746b
hobbitcakes <hobbitcakes@gmail.com>
2018-01-18 02:31:51 +0000

    Adding script to install go

--- tinydictionary.go
+++ tinydictionary.go
@@ -1,11 +1,8 @@
 package main
 
-import "fmt"
-
 import "github.com/gin-gonic/gin"
 
-func main() 
-{
+func main() {
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
