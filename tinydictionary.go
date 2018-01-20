9b23878c15dd1c03ccead733822b1d3d1ba4b24d
Jared Peterson <hobbitcakes@gmail.com>
2018-01-20 16:05:53 -0600

    adding simple test script

--- tinydictionary.go
+++ tinydictionary.go
@@ -5,33 +5,36 @@ import "github.com/gin-gonic/gin"
 import "net/http"
 
 func main() {
+	// Create a gin router with the default middleware
 	router := gin.Default()
-	// Simulate a ping/pong request
-	router.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
+
+	// Simulate a ping request
+	router.GET("/ping", func(context *gin.Context) {
+		context.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
+
 	// Simulate a file upload
 	// Example: curl -X POST -F "file=@/full/path/to/file.txt" -H "Content-Type: multipart/form-data" http://<hostname>:9999/dinosaurs
-	router.POST("/dinosaurs", func(c *gin.Context) {
+	router.POST("/dinosaurs", func(context *gin.Context) {
 
 		// Source
-		file, err := c.FormFile("file")
+		file, err := context.FormFile("file")
 		// Verify request
 		if err != nil {
-			c.String(http.StatusBadRequest, fmt.Sprint("get form err: %s", err.Error()))
+			context.String(http.StatusBadRequest, fmt.Sprint("get form err: %s", err.Error()))
 			return
 		}
 
 		// Save the file uploaded to /dev/null
 		// Change "/dev/null" to file.Filename to save the file to the local directory
-		if err := c.SaveUploadedFile(file, "/dev/null"); err != nil {
-			c.String(http.StatusBadRequest, fmt.Sprintf("upload file err: %s", err.Error()))
+		if err := context.SaveUploadedFile(file, "/dev/null"); err != nil {
+			context.String(http.StatusBadRequest, fmt.Sprintf("upload file err: %s", err.Error()))
 			return
 		}
 
-		c.String(http.StatusOK, fmt.Sprint("File %s uploaded successfully!", file.Filename))
+		context.String(http.StatusOK, fmt.Sprintf("File %s uploaded successfully!\n", file.Filename))
 	})
 
 	router.Run(":9999") // listen and serve on 0.0.0.0:9999
